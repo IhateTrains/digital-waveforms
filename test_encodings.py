ddbc321e3fffcab3569bd6d91e5f02cb5d3e0097
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-30 23:41:39 +0200

    Update test_encodings.py

--- test_encodings.py
+++ test_encodings.py
@@ -1,10 +1,6 @@
 from manchester import manchester
 
 
-def test_NRZ():
-    assert manchester([0,1,1,1,1,0,0,1], 0, 1) == [0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0]
-    assert manchester([1,0,1,0,1], 0, 1) == [1,0,0,1,1,0,0,1,1,0]
-
 def test_manchester():
     assert manchester([0,1,1,1,1,0,0,1], 0, 1) == [0,1,1,0,1,0,1,0,1,0,0,1,0,1,1,0]
     assert manchester([1,0,1,0,1], 0, 1) == [1,0,0,1,1,0,0,1,1,0]
