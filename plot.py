c0c90bc0e834b68774f5605861d5ccacdf7cf60d
Szymon Rąpała <58051054+SzymiDjud@users.noreply.github.com>
2021-05-30 23:12:07 +0200

    Add files via upload

--- plot.py
+++ plot.py
@@ -23,3 +23,18 @@ def plot(bity, napiecia, niski, wysoki):
         plt.text(tbit + 0.5, wysoki + 0.5, str(bit))
 
     plt.show()
+
+def plot1(bity, napiecia, poziom):
+    dlugosc = len(bity) + 2
+    t = dlugosc/len(napiecia) * np.arange(len(napiecia))
+    #t = len(napiecia)
+    my_lines('x', range(dlugosc), color='.5', linewidth=1, linestyle='--')
+    my_lines('y', [(-3*poziom),0,(3*poziom)], color='.5', linewidth=1, linestyle='--')
+    plt.step(t, napiecia, 'r', linewidth=2, where='post')
+    plt.ylim([(-3*poziom)-1,(3*poziom)+1])
+    plt.xlim(0, dlugosc)
+
+    for tbit, bit in enumerate(bity):
+        plt.text(tbit + 0.5, poziom + 0.5, str(bit))
+
+    plt.show()
