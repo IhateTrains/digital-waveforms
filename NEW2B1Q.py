fb78f52f115c2a625955acf5b87c08cb455cf837
Szymon Rąpała <58051054+SzymiDjud@users.noreply.github.com>
2021-06-05 23:03:26 +0200

    NOWY
    Dzieki działa

---
+++ NEW2B1Q.py
@@ -0,0 +1,49 @@
+from plot import plot, plot1
+from helpers import split
+
+
+def plot_AMI(ciag):
+
+    bity = ciag
+    level = float(input('Podaj poziom (Jeżeli nie wiesz/nie masz podane wpisz 1): '))
+
+    napiecia = []
+
+    flag = False
+    a = ""
+    b1q = []
+    temp = []
+    b = 0
+    for bit in bity:
+        a += bit
+        b += 1
+        if b == 2:
+            b = 0
+            b1q.append(a)
+            a = ""
+
+    for i in range(0,len(b1q)):
+        if b1q[i] == '00':
+            napiecia.append((-3*level))
+            ostatni = -3*level
+        elif b1q[i] == '01':
+            napiecia.append((-1*level))
+            ostatni = -1*level
+        elif b1q[i] == '10':
+            napiecia.append((3*level))
+            ostatni = 3*level
+        elif b1q[i] == '11':
+            napiecia.append((1*level))
+            ostatni = 1*level
+
+    wart_srednia = sum(napiecia)/len(napiecia)
+    napiecia.append(ostatni)
+    plot1(bity,napiecia,level)
+
+
+    print('Srednia wartosc sygnalu:', wart_srednia)
+    #print("Napiecia: ",napiecia)
+    #print("Dlugosc: ",len(napiecia))
+    #print("Suma: ",sum(napiecia))
+
+plot_AMI('101100001101')
